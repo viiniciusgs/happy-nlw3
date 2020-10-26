0b8f031905fb21ec96ef3fef4a3b61757f51f755
Vinicius Gabriel <viiniciusgs03@gmail.com>
2020-10-26 13:37:57 -0300

    first version

---
+++ src/database/fakedata.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-23.0944558",
+        lng: "-47.7078509",
+        name: "Casa dos Meninos",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1582167371270-68a4c033e732?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1590009617786-6d054a2a3c7c?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "1"
+    },
+    {
+        id: 2,
+        lat: "-23.0944558",
+        lng: "-47.7178509",
+        name: "Casa das Meninas",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1582167371270-68a4c033e732?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1590009617786-6d054a2a3c7c?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "0"
+    }
+]
---
+++ src/database/test.js
@@ -0,0 +1,32 @@
+const Database = require("./db");
+const saveOrphanage = require("./saveOrphanage");
+
+Database.then(async (db) => {
+  // inserir dados na tabela
+/*   await saveOrphanage(db, {
+    lat: "-23.0944558",
+    lng: "-47.7178509",
+    name: "Casa das Meninas",
+    about: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    whatsapp: "991566851",
+    images: [
+      "https://images.unsplash.com/photo-1582167371270-68a4c033e732?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+      "https://images.unsplash.com/photo-1590009617786-6d054a2a3c7c?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+    ].toString(),
+    instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Das 18h até 8h",
+    open_on_weekends: "0"
+  }); */
+  
+  // consultar dados na tabela
+  const selectedOrphanages = await db.all("SELECT * FROM orphanages");
+  console.log(selectedOrphanages);
+
+/*   // consultar somente 1 orfanato, pelo id
+  const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "3"');
+  console.log(orphanage); */
+
+  // deletar dado da tabela
+/*   console.log(await db.run('DELETE FROM orphanages WHERE id ="13"')) */
+});
